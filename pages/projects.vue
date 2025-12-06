4f71574594d77cb408a22a1e57082136bc13f17b
Raghav Misra <raghav.m2014@gmail.com>
2025-12-06 03:34:17 -0500

    replace with nothing

--- pages/projects.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { projects } from "~/content/projects";
-</script>
-
-<template>
-  <div class="main">
-    <ProjectSection v-for="project of projects" :project="project" />
-  </div>
-</template>
